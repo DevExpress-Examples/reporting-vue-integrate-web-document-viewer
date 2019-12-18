e5e676d217826c6efc33cc87790e941679aad8bb
BrianDX <boris.zaitsev@devexpress.com>
2019-12-18 19:39:44 +0300

    example content

---
+++ JS/vue-report-viewer/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <WebDocumentViewer />
+</div>
+</template>
+
+<script>
+import WebDocumentViewer from './components/WebDocumentViewer';
+
+export default {
+name: 'app',
+components: {
+    WebDocumentViewer
+}
+}
+</script>
---
+++ JS/vue-report-viewer/src/components/WebDocumentViewer.vue
@@ -0,0 +1,28 @@
+<template>
+<div ref="viewer" style="position: absolute; left: 0; right: 0; top: 0; bottom: 0" data-bind="dxReportViewer: $data"></div>
+</template>
+
+<script>
+import ko from "knockout";
+import "devexpress-reporting/dx-webdocumentviewer";
+
+export default {
+name: "WebDocumentViewer",
+mounted() {
+    var viewerOptions = {
+        reportUrl: ko.observable("Products"), // The URL of a report that is opened in the Report Designer when the application starts.
+        requestOptions: {
+        host: "http://localhost:54114/",
+	// Use this line if you use an ASP.NET MVC backend
+	invokeAction: "/WebDocumentViewer/Invoke"
+	// Uncomment this line if you use an ASP.NET Core backend
+	//invokeAction: "DXXRDV"
+        }
+    };
+    ko.applyBindings(viewerOptions, this.$refs.viewer);
+},
+beforeDestroy() {
+    ko.cleanNode(this.$refs.viewer);
+}
+};
+</script>
