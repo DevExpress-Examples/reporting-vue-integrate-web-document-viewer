2501242847db197ce13de51d7387ece36dad3550
Boris Zaitsev <boris.zaitsev@devexpress.com>
2023-02-21 16:05:21 +0400

    fix ports on server-side app

--- CS/ServerSideApp/Startup.cs
+++ CS/ServerSideApp/Startup.cs
@@ -25,6 +25,7 @@ namespace ServerSideApp {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddDevExpressControls();
+            services.AddControllersWithViews();
             services.AddScoped<ReportStorageWebExtension, CustomReportStorageWebExtension>();
             services
                 .AddMvc()
