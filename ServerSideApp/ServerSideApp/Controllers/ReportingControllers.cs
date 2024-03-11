f12e8b8372adf8a0674786a7d5cbfbb72493ee3b
Polina Tyureva <polina.tyureva@devexpress.com>
2024-03-11 14:20:06 +0400

    update example

---
+++ ServerSideApp/ServerSideApp/Controllers/ReportingControllers.cs
@@ -0,0 +1,10 @@
+﻿using DevExpress.AspNetCore.Reporting.WebDocumentViewer;
+using DevExpress.AspNetCore.Reporting.WebDocumentViewer.Native.Services;
+
+namespace  ServerSideApp.Controllers {
+
+    public class CustomWebDocumentViewerController : WebDocumentViewerController {
+        public CustomWebDocumentViewerController(IWebDocumentViewerMvcControllerService controllerService) : base(controllerService) {
+        }
+    }
+}
